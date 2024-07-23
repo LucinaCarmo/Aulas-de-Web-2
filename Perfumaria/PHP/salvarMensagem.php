26e3ee58e09ae966dc147abb0bd7fa911ac08238
LucinaCarmo <lucinagdocarmo@gmail.com>
2024-07-23 11:54:46 -0300

    revisão
    23-07

--- Perfumaria/PHP/salvarMensagem.php
+++ Perfumaria/PHP/salvarMensagem.php
@@ -6,7 +6,20 @@ $assunto = filter_input(INPUT_POST, 'assunto', FILTER_SANITIZE_STRING);
 $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
 
+$salvar = "INSERT INTO `mensagem`(`nome`,`assunto`,`mensagem`,`email`)VALUES ('$nome', '$assunto', '$mensagem', '$email')";
 
+$resultado = mysqli_query($conexao,$salvar);
+
+if($resultado){
+    echo"<script>
+    window.location.href = '../Contato.html?status=success';
+    </script>";
+}else{
+
+    echo "<script> 
+    window.location.href = '../Contato.html?status=error';
+    </script>";
+}
 
 
 ?>
